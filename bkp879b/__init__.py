ce74ae2e1b346d83eb9c7265ea5bb0a7cb3c0a65
Jim Battin <jrbattin@gmail.com>
2016-03-26 11:25:06 -0500

    Making this installable via setuptools

---
+++ bkp879b/__init__.py
@@ -0,0 +1,4 @@
+from scpi import *
+
+
+__version__ = '1.0.0'
--- tests.py
+++ tests.py
@@ -1,113 +1,117 @@
-""" Unit tests for bkp879b.py """
+''' Unit tests for bkp879b.py '''
 import unittest
 import bkp879b
 from serial import SerialException
 
 
 def dummy_sendcmd(command):
-    """ Dummy sendcmd method for testing SCPI command generation """
-    return "{0}\n".format(command)
+    ''' Dummy sendcmd method for testing SCPI command generation '''
+    return '{0}\n'.format(command)
 
 
 class TestConnect(unittest.TestCase):
-    """ Test cases for the connect function """
+    ''' Test cases for the connect function '''
 
     def setUp(self):
-        """ Test setup """
+        ''' Test setup '''
         self.bkp = bkp879b
-        self.goodserial = "/dev/tty.SLAB_USBtoUART"
-        self.badserial = "/dev/null"
+        self.goodserial = '/dev/tty.SLAB_USBtoUART'
+        self.badserial = '/dev/null'
 
     def test_goodconnect(self):
-        """ Test to ensure our 'happy path' of a valid serial connection.
-            Assumes self.goodserial is set to a valid serial device """
+        ''' Test to ensure our 'happy path' of a valid serial connection.
+            Assumes self.goodserial is set to a valid serial device '''
         scpi_obj = self.bkp.connect(self.goodserial)
         self.assertTrue(isinstance(scpi_obj, bkp879b.ScpiConnection))
 
     def test_badconnect(self):
-        """ Test to ensure our 'unhappy path' is producing the correct
+        ''' Test to ensure our 'unhappy path' is producing the correct
             exception. Assumes self.badserial is set to an invalid
-            serial device. """
+            serial device. '''
         with self.assertRaises(SerialException):
             self.bkp.connect(self.badserial)
 
 
 class TestParse(unittest.TestCase):
-    """ Test cases for the parse function """
+    ''' Test cases for the parse function '''
 
     def setUp(self):
-        """ Test setup """
+        ''' Test setup '''
         self.bkp = bkp879b
 
     def test_integers(self):
-        """ Tests integer parsing """
-        ints = self.bkp.parse(b"+800,-900234,0,27".encode("ascii"))
+        ''' Tests integer parsing '''
+        ints = self.bkp.parse(b'+800,-900234,0,27'.encode('ascii'))
         self.assertTupleEqual(ints, (800, -900234, 0, 27))
 
     def test_floats(self):
-        """ Tests float parsing """
-        floats = self.bkp.parse("+2.345678e+04,-1.34567e-01".encode("ascii"))
+        ''' Tests float parsing '''
+        floats = self.bkp.parse('+2.345678e+04,-1.34567e-01'.encode('ascii'))
         self.assertTupleEqual(floats, (23456.78, -0.134567))
 
     def test_booleans(self):
-        """ Tests boolean parsing """
-        bools = self.bkp.parse("ON,OFF".encode("ascii"))
+        ''' Tests boolean parsing '''
+        bools = self.bkp.parse('ON,OFF'.encode('ascii'))
         self.assertTupleEqual(bools, (True, False))
 
     def test_literals(self):
-        """ Tests string literal parsing """
-        literals = self.bkp.parse("HOLD,TESTING".encode("ascii"))
-        self.assertTupleEqual(literals, ("HOLD", "TESTING"))
+        ''' Tests string literal parsing '''
+        literals = self.bkp.parse('HOLD,TESTING'.encode('ascii'))
+        self.assertTupleEqual(literals, ('HOLD', 'TESTING'))
 
     def test_blanks(self):
-        """ Tests blank '----' results """
-        blanks = self.bkp.parse("----,N".encode("ascii"))
+        ''' Tests blank '----' results '''
+        blanks = self.bkp.parse('----,N'.encode('ascii'))
         self.assertTupleEqual(blanks, (None, None))
 
     def test_single(self):
-        """ Tests non-tuple single result """
-        single_int = self.bkp.parse("123".encode("ascii"))
-        single_float = self.bkp.parse("+2.345678e+04".encode("ascii"))
-        single_none = self.bkp.parse("N".encode("ascii"))
-        single_bool = self.bkp.parse("ON".encode("ascii"))
-        single_literal = self.bkp.parse("HOLD".encode("ascii"))
-        self.assertTupleEqual((single_int, single_float, single_none, single_bool,
-            single_literal), (123, 23456.78, None, True, "HOLD"))
+        ''' Tests non-tuple single result '''
+
+        single_int = self.bkp.parse('123'.encode('ascii'))
+        single_float = self.bkp.parse('+2.345678e+04'.encode('ascii'))
+        single_none = self.bkp.parse('N'.encode('ascii'))
+        single_bool = self.bkp.parse('ON'.encode('ascii'))
+        single_literal = self.bkp.parse('HOLD'.encode('ascii'))
+
+        self.assertTupleEqual(
+            (single_int, single_float, single_none, single_bool, single_literal),
+            (123, 23456.78, None, True, 'HOLD')
+        )
 
 
 class TestScpiConnection(unittest.TestCase):
-    """ Tests SCPI Connection object - specifically the command output """
+    ''' Tests SCPI Connection object - specifically the command output '''
 
     def setUp(self):
-        """ Sets up our ScpiConnection object with sendcmd dummy object """
+        ''' Sets up our ScpiConnection object with sendcmd dummy object '''
         self.scpi = bkp879b.ScpiConnection(None, 100)
         self.scpi.sendcmd = dummy_sendcmd
 
     def test_badfrequency(self):
-        """ Ensures appropriate exception is thrown for set_frequency. """
+        ''' Ensures appropriate exception is thrown for set_frequency. '''
 
         with self.assertRaises(bkp879b.ScpiException):
             self.scpi.set_frequency(90)
 
     def test_badprimary(self):
-        """ Ensures appropriate exception is thrown for set_primary. """
+        ''' Ensures appropriate exception is thrown for set_primary. '''
 
         with self.assertRaises(bkp879b.ScpiException):
-            self.scpi.set_primary("THETA")
+            self.scpi.set_primary('THETA')
 
     def test_badsecondary(self):
-        """ Ensures appropriate exception is thrown for set_primary. """
+        ''' Ensures appropriate exception is thrown for set_primary. '''
 
         with self.assertRaises(bkp879b.ScpiException):
-            self.scpi.set_secondary("L")
+            self.scpi.set_secondary('L')
 
     def test_badtolerance(self):
-        """ Ensures appropriate exception is thrown for
-        set_tolerance_range. """
+        ''' Ensures appropriate exception is thrown for
+        set_tolerance_range. '''
 
         with self.assertRaises(bkp879b.ScpiException):
             self.scpi.set_tolerance_range(3)
 
 
-if __name__ == "__main__":
+if __name__ == '__main__':
     unittest.main()
